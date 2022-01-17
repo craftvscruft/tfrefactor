9650d15b3d6f190a115e60d3a4a46c025bd86e90
Ray Myers <ray.myers@divvydose.com>
2022-01-17 17:53:37 -0600

    Bypass approval with -f

--- cmd/rename.go
+++ cmd/rename.go
@@ -29,6 +29,7 @@ Arguments:
 	}
 	flags := cmd.Flags()
 	flags.StringP("config", "c", "-", "Path of terraform to modify, defaults to current.")
+	flags.BoolP("force", "f", false, "Skip interactive approval of update before applying")
 
 	return cmd
 }
@@ -49,6 +50,7 @@ func runRenameCmd(cmd *cobra.Command, args []string) error {
 	toAddress := args[1]
 	configPath, err := cmd.Flags().GetString("config")
 	CheckFatal(err)
+
 	if configPath == "-" {
 		configPath, err = os.Getwd()
 		CheckFatal(err)
@@ -60,21 +62,43 @@ func runRenameCmd(cmd *cobra.Command, args []string) error {
 		return err
 	}
 	plan, err := refactor.Rename(fromAddress, toAddress, configPath)
+	if err != nil {
+		return err
+	}
+	err = approveAndApplyUpdate(cmd, plan)
+	return err
+}
+
+func approveAndApplyUpdate(cmd *cobra.Command, plan *refactor.UpdatePlan) error {
+	autoApprove, err := cmd.Flags().GetBool("force")
+	CheckFatal(err)
 	if len(plan.FileUpdates) > 0 {
-		_, err = fmt.Fprintf(cmd.OutOrStdout(), "Update %v file(s)? [y/N]: ", len(plan.FileUpdates))
-		CheckFatal(err)
-		var in string
-		_, err = fmt.Fscanf(cmd.InOrStdin(), "%s", &in)
-		CheckFatal(err)
-		if in == "Y" || in == "y" {
+		if autoApprove {
 			err = applyUpdate(plan)
 			CheckFatal(err)
-		} else {
-			_, err = fmt.Fprintf(cmd.OutOrStdout(), "\nAborted\n")
+			_, err = fmt.Fprintln(cmd.OutOrStdout(), "Done.")
 			CheckFatal(err)
+		} else {
+			_, err = fmt.Fprintf(cmd.OutOrStdout(), "Update %v file(s)? [y/N]: ", len(plan.FileUpdates))
+			CheckFatal(err)
+			var in string
+			_, _ = fmt.Fscanln(cmd.InOrStdin(), &in)
+			// Ignore Fscanln err because empty input is OK.
+			if in == "Y" || in == "y" {
+				err = applyUpdate(plan)
+				CheckFatal(err)
+				_, err = fmt.Fprintln(cmd.OutOrStdout(), "Done.")
+				CheckFatal(err)
+			} else {
+				_, err = fmt.Fprintf(cmd.OutOrStdout(), "\nAborted.\n")
+				CheckFatal(err)
+			}
 		}
+	} else {
+		_, err = fmt.Fprintf(cmd.OutOrStdout(), "\nNo updates required.\n")
+		CheckFatal(err)
 	}
-	return err
+	return nil
 }
 
 func applyUpdate(plan *refactor.UpdatePlan) error {
